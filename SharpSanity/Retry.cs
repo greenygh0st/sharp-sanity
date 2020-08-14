f3810fdd052f08e24948e6d0b28427d9711c9c23
Dale Myszewski II <dale.myszewski@gmail.com>
2020-08-14 16:00:03 -0500

    Added sync retry method and tsts

--- SharpSanity/Retry.cs
+++ SharpSanity/Retry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SharpSanity.FaultTolerance
@@ -33,6 +34,37 @@ namespace SharpSanity.FaultTolerance
                         throw new AggregateException(exceptions);
 
                     await Task.Delay(TimeSpan.FromMilliseconds(sleepPeriod.TotalMilliseconds * attempts));
+                }
+            }
+        }
+
+        public static void Go(Func<object> action, TimeSpan sleepPeriod, int tryCount = 5)
+        {
+            List<Exception> exceptions = new List<Exception>();
+
+            if (tryCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(tryCount));
+
+            int attempts = 0;
+
+            while (true)
+            {
+                attempts++;
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    // keep a list of all the exceptions (cause they might be different)
+                    exceptions.Add(ex);
+
+                    // if we have run out of tries then error out
+                    if (--tryCount == 0)
+                        throw new AggregateException(exceptions);
+
+                    Thread.Sleep(TimeSpan.FromMilliseconds(sleepPeriod.TotalMilliseconds * attempts));
                 }
             }
         }
