3140992d8e39dc0cb79110b5301d440fd1c3f7d0
Dale Myszewski II <dale.myszewski@gmail.com>
2020-11-04 11:24:38 -0600

    Added truncate

---
+++ SharpSanity/Truncate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SharpSanity.Truncate
+{
+    public static class TruncateExtension
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}
